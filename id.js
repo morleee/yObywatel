780a8784ab377b00cca7b95a677452e8d0fa047b
morleee <morleee31@gmail.com>
2024-09-17 02:20:22 +0200

    Add files via upload

--- id.js
+++ id.js
@@ -6,17 +6,18 @@ var firstname = params.get("firstname");
 var surname = params.get("surname");
 var image = params.get("image");
 
-function launchFullScreen(element) {
-    if(element.requestFullScreen) {
-      element.requestFullScreen();
-    } else if(element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-    } else if(element.webkitRequestFullScreen) {
-      element.webkitRequestFullScreen();
-    }
-  }
-  
-launchFullScreen(document.documentElement);
+let webManifest = {
+  "name": "",
+  "short_name": "",
+  "theme_color": "#f5f6fb",
+  "background_color": "#f5f6fb",
+  "display": "standalone"
+};
+
+let manifestElem = document.createElement('link');
+manifestElem.setAttribute('rel', 'manifest');
+manifestElem.setAttribute('href', 'data:application/manifest+json;base64,' + btoa(JSON.stringify(webManifest)));
+document.head.prepend(manifestElem);
 
 document.querySelector(".surname").innerHTML = surname.toUpperCase();
 document.querySelector(".firstname").innerHTML = firstname.toUpperCase();
