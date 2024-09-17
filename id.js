9d2c924ef8369acd64662a4a31f1156146d1df95
morleee <morleee31@gmail.com>
2024-09-17 02:10:48 +0200

    Add files via upload

--- id.js
+++ id.js
@@ -6,6 +6,18 @@ var firstname = params.get("firstname");
 var surname = params.get("surname");
 var image = params.get("image");
 
+function launchFullScreen(element) {
+    if(element.requestFullScreen) {
+      element.requestFullScreen();
+    } else if(element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    }
+  }
+  
+launchFullScreen(document.documentElement);
+
 document.querySelector(".surname").innerHTML = surname.toUpperCase();
 document.querySelector(".firstname").innerHTML = firstname.toUpperCase();
 document.querySelector(".id_own_image").style.backgroundImage = "url('" + image + "')";
