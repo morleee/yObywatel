7544181fa4ca513e93b74714276321272a85befd
morleee <morleee31@gmail.com>
2024-09-19 21:07:09 +0200

    Add files via upload

--- id.js
+++ id.js
@@ -7,6 +7,14 @@ var firstname = params.get("firstname");
 var surname = params.get("surname");
 var image = params.get("image");
 
+function hideAddressBar(){
+  if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
+    document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
+  setTimeout(window.scrollTo(1,1),0);
+}
+window.addEventListener("load",function(){hideAddressBar();});
+window.addEventListener("orientationchange",function(){hideAddressBar();});
+
 let webManifest = {
   "name": "",
   "short_name": "",
